50e843d97cb8a37661a377fce1238af4fc4e5e51
alez04 <alez2106x@gmail.com>
2026-01-09 11:44:17 +0200

    chore(crypto): organize exports

--- src/crypto/schnorrq.ts
+++ src/crypto/schnorrq.ts
@@ -1,2 +1,2 @@
-export { generatePublicKey, sign, verify } from "@qubic-labs/schnorrq";
 export type { CreateSchnorrqParams, Schnorrq, SchnorrqSigner } from "@qubic-labs/schnorrq";
+export { generatePublicKey, sign, verify } from "@qubic-labs/schnorrq";
