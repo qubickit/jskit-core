e9393cd6578a8799583f4c92f05ae63b2f9844be
alez04 <alez2106x@gmail.com>
2026-01-13 02:23:42 +0200

    chore: export transaction params type

--- index.ts
+++ index.ts
@@ -70,6 +70,7 @@ export {
 export {
   buildSignedTransaction,
   buildUnsignedTransaction,
+  signTransaction,
   MAX_INPUT_SIZE,
   MAX_TRANSACTION_SIZE,
   SIGNATURE_LENGTH,
@@ -78,6 +79,7 @@ export {
   transactionId,
   unsignedTransactionDigest,
 } from "./src/transactions/transaction.js";
+export type { BuildUnsignedTransactionParams } from "./src/transactions/transaction.js";
 export { createBridgeTransport } from "./src/transport/bridge.js";
 export { createTcpTransport } from "./src/transport/tcp.js";
 export type { Transport } from "./src/transport/transport.js";
